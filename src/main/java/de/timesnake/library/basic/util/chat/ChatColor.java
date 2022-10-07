560602115f9520b2b8b79f6262d334a52be8a320
funzter <funzter@mail.timesnake.de>
2022-10-08 01:19:46 +0200

    added copyright

--- src/main/java/de/timesnake/library/basic/util/chat/ChatColor.java
+++ src/main/java/de/timesnake/library/basic/util/chat/ChatColor.java
@@ -1,14 +1,28 @@
+/*
+ * library-basic.main
+ * Copyright (C) 2022 timesnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.timesnake.library.basic.util.chat;
 
 public interface ChatColor {
 
-    String PUBLIC = ChatColor.WHITE;
-    String PERSONAL = ChatColor.YELLOW;
-    String VALUE = ChatColor.GRAY;
-    String WARNING = ChatColor.RED;
-    String QUICK_INFO = ChatColor.BLACK;
     String SEPARATOR = "----------";
     String BLACK = "§0";
+    String QUICK_INFO = ChatColor.BLACK;
     String DARK_BLUE = "§1";
     String DARK_GREEN = "§2";
     String DARK_AQUA = "§3";
@@ -16,14 +30,18 @@ public interface ChatColor {
     String DARK_PURPLE = "§5";
     String GOLD = "§6";
     String GRAY = "§7";
+    String VALUE = ChatColor.GRAY;
     String DARK_GRAY = "§8";
     String BLUE = "§9";
     String GREEN = "§a";
     String AQUA = "§b";
     String RED = "§c";
+    String WARNING = ChatColor.RED;
     String LIGHT_PURPLE = "§d";
     String YELLOW = "§e";
+    String PERSONAL = ChatColor.YELLOW;
     String WHITE = "§f";
+    String PUBLIC = ChatColor.WHITE;
     String MAGIC = "§k";
     String BOLD = "§l";
     String STRIKETHROUGH = "§m";
