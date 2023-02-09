1aa387054b772fa80d397dd07cd8a60f906df110
funzter <funzter@mail.timesnake.de>
2023-02-09 22:28:12 +0100

    changed command code system

--- src/main/java/de/timesnake/library/basic/util/server/Server.java
+++ src/main/java/de/timesnake/library/basic/util/server/Server.java
@@ -6,6 +6,8 @@ package de.timesnake.library.basic.util.server;
 
 public interface Server {
 
+    String NETWORK_NAME = "TimeSnake";
+
     String WEBSITE_LINK = "https://timesnake.de";
     String SUPPORT_EMAIL = "support@mail.timesnake.de";
     String DISCORD_LINK = "https://discord.gg/YRCZhFVE9z";
