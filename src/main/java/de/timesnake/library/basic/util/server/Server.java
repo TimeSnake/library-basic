72879e44eba22514a4b4ad5d701b8f20c67cefd4
funzter <funzter@mail.timesnake.de>
2023-05-25 17:50:42 +0200

    minor

--- src/main/java/de/timesnake/library/basic/util/server/Server.java
+++ src/main/java/de/timesnake/library/basic/util/server/Server.java
@@ -6,12 +6,21 @@ package de.timesnake.library.basic.util.server;
 
 public interface Server {
 
-  String NETWORK_NAME = "TimeSnake";
+  String DEFAULT_NETWORK_NAME = "TimeSnake";
+  String CREATOR_LINK = "https://timesnake.de";
+  String CREATOR_NAME = "TimeSnake";
 
+  @Deprecated
   String WEBSITE_LINK = "https://timesnake.de";
+  @Deprecated
   String SUPPORT_EMAIL = "support@mail.timesnake.de";
+  @Deprecated
   String DISCORD_LINK = "https://discord.gg/YRCZhFVE9z";
+  @Deprecated
   String PATREON_LINK = "https://www.patreon.com/user?u=59222684";
+  @Deprecated
   String DEFAULT_TEXTURE_PACK = "https://api.timesnake.de/data/minecraft/resource_packs/texture_pack_default.zip";
+  @Deprecated
+  String YOUTUBE_LINK = "https://www.youtube.com/@timesnakede";
 
 }
